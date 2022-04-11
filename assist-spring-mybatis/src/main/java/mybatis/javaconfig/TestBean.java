e455deae47390bc2722ee2bc40103266d051c2a6
dslztx <dslztx@gmail.com>
2022-04-11 17:24:29 +0800

    从assist_springboot迁移assist-spring-zookeeper

---
+++ assist-spring-mybatis/src/main/java/mybatis/javaconfig/TestBean.java
@@ -0,0 +1,9 @@
+package mybatis.javaconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = "mybatis")
+public class TestBean {
+}
